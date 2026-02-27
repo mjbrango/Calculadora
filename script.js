b35db0dc5d8fde9ac9f23a23403ad51171bdedf3
mariaJose <majobrango2002@gmail.com>
2026-02-26 22:07:22 -0500

    Primer Comimit

---
+++ script.js
@@ -0,0 +1,21 @@
+let pantalla = document.getElementById("pantalla");
+
+function agregar(valor){
+    pantalla.value += valor;
+}
+
+function limpiar(){
+    pantalla.value = "";
+}
+
+function calcular(){
+    try{
+        pantalla.value = eval(pantalla.value);
+    }catch{
+        pantalla.value = "Error";
+    }
+}
+
+function porcentaje(){
+    pantalla.value = eval(pantalla.value)/100;
+}
