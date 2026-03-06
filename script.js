176a9c9c01a893c1883ca04631039f829260678c
mariaJose <majobrango2002@gmail.com>
2026-03-05 20:12:20 -0500

    Se le agrego el Historial y un boton de borrar

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
 let pantalla = document.getElementById("pantalla");
+let historial = document.getElementById("historial");
 
 function agregar(valor){
-    pantalla.value += valor;
+pantalla.value += valor;
 }
 
 function limpiar(){
-    pantalla.value = "";
+pantalla.value = "";
 }
 
-function calcular(){
-    try{
-        pantalla.value = eval(pantalla.value);
-    }catch{
-        pantalla.value = "Error";
-    }
+function borrarUno(){
+pantalla.value = pantalla.value.slice(0,-1);
 }
 
 function porcentaje(){
-    pantalla.value = eval(pantalla.value)/100;
+pantalla.value = eval(pantalla.value) / 100;
 }
+
+function calcular(){
+
+try{
+
+let operacion = pantalla.value;
+let resultado = eval(operacion);
+
+pantalla.value = resultado;
+
+let item = document.createElement("li");
+item.textContent = operacion + " = " + resultado;
+
+historial.appendChild(item);
+
+}catch{
+
+pantalla.value = "Error";
+
+}
+
+}
+
+document.addEventListener("keydown",function(event){
+
+if(!isNaN(event.key) || "+-*/.".includes(event.key)){
+pantalla.value += event.key;
+}
+
+if(event.key==="Enter"){
+calcular();
+}
+
+if(event.key==="Backspace"){
+pantalla.value = pantalla.value.slice(0,-1);
+}
+
+});
